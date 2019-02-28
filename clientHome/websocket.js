baf424a388b787b071c15cee6504bf04fc1c1505
spoutnik911 <34311347+spoutnik911@users.noreply.github.com>
2019-02-28 13:07:19 +0100

    delete

--- clientHome/websocket.js
+++ clientHome/websocket.js
@@ -1,34 +1,39 @@
 var ws = new WebSocket("ws://localhost:8080");
 
-var pseudo = "truc";
+window.pseudo = "truc";
 
-ws.onmessage = function(event){
-    console.log(event.data);
-    onReceivMessage(event.data);
+ws.onmessage = function(event) {
+  console.log(event.data);
+  onReceivMessage(event.data);
+};
+
+function onsendmsg(event) {
+  if (event.keyCode === 13 && pseudo !== "nothing") {
+    sendMsgtoServ(document.getElementById("textbox").value, pseudo);
+  }
 }
 
-function onsendmsg(event){
+function sendMsgtoServ(msg, pseudo) {
+  var cache = {
+    msg: msg,
+    pseudo: pseudo
+  };
 
-    if (event.keyCode === 13 && pseudo !== "nothing") {
-        sendMsgtoServ(document.getElementById('textbox').value, pseudo);
-    }
+  if (cache.msg.includes("/nick")){
+    let pseudoSplit = cache.msg.split(" ");
+    cache.pseudo = pseudoSplit[1];
+    window.pseudo = pseudoSplit[1];
+  }
 
+
+  cache = JSON.stringify(cache);
+  onReceivMessage(cache);
+  ws.send(cache);
 }
 
-function sendMsgtoServ(msg, pseudo){
-
-    var cache = {
-        msg: msg,
-        pseudo: pseudo
-    };
-
-    cache = JSON.stringify(cache);
-    ws.send(cache);
-}
-
-function onReceivMessage(msg){
-    var message = document.createElement("div");
-    message.innerText = JSON.parse(msg).pseudo+ ":" + JSON.parse(msg).msg;
-    message.className = "message_div";
-    document.getElementsByClassName('contentbox')[0].insertBefore(message, document.getElementById('endcontentbox'));
+function onReceivMessage(msg) {
+  var message = document.createElement("div");
+  message.innerText = JSON.parse(msg).pseudo + ":" + JSON.parse(msg).msg;
+  message.className = "message_div";
+  document.getElementsByClassName("contentbox")[0].insertBefore(message, document.getElementById("endcontentbox"));
 }
